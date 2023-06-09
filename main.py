ba4305ac84e7d339fadf45864c674bc62cae506a
jeny <jenymontiel13@gmail.com>
2023-06-09 13:24:13 -0600

    02 Variable

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # function print
 print("Hola<3")
+#Podemos enviar distintos tipos de dato
+print("Un string")
+print("un entero",25)
+print("un float",3.14)
+print("un boolean",True)
+
+
+
---
+++ variable.py
@@ -0,0 +1,11 @@
+#Python es no Tipado
+name ="Jenysita"
+age = 19
+height= 1.68
+is_student= True
+children = [] 
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
