660b3cfe85a3d3de0542ae94ee603d4dacf79d67
jeny <jenymontiel13@gmail.com>
2023-06-12 09:38:27 -0600

    04Operadores

---
+++ operadores.py
@@ -0,0 +1,17 @@
+#Operadores Matematicas
+# + - * %
+# / división float
+# / División entera
+# ** Potencia
+print (2 + 2)
+print (2 - 2)
+print (2 * 2)
+print (2 % 2)
+print (2 / 2)
+print (2 // 2)
+print (2 ** 2)
+
+#Operadores para Strings
+# + (concatena) * (replica)
+print("hola " + "Mundo!")
+print("Hola " *4) 
