ed780b781303a049049785cc420ce3805412a297
jeny <jenymontiel13@gmail.com>
2023-06-13 14:58:48 -0600

    08 Listas

---
+++ colecciones-1.py
@@ -0,0 +1,22 @@
+#Listas
+#Entre corchetes []
+my_list =  [123, True, 3.14, "Holi"]
+print(type(my_list))
+print(my_list)
+
+#Indexing
+print(my_list[0])
+
+#Slicing
+print(my_list[2:])
+
+students = ["Jenyfer", "Alicia", "Jesersin"]
+print(students)
+students.append("Paco")
+students.append("Nacho")
+students.append("Gaby")s
+print(students)
+students.remove("Jenyfer")
+print(students)
+students[1]="Verito"
+print(students)
