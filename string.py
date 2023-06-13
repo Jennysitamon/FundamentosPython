97ed91a055620b33c36adf757753374100b20efe
jeny <jenymontiel13@gmail.com>
2023-06-13 14:16:37 -0600

    07 Strings

---
+++ string.py
@@ -0,0 +1,21 @@
+my_string = " h0l4 mUnDo! "
+#Mayusculas
+print(my_string.upper())
+#Minusculas
+print(my_string.lower())
+#Longitud de String
+print(len(my_string))
+#Quitar espacios inicio y fin
+print(my_string.strip())
+
+my_string = "Este es un nuevo texto"
+
+# Indexing and slicing
+print(my_string[0])
+print(my_string[3])
+print(my_string[6])
+
+print(my_string [0:5])
+print(my_string [2:5])
+print(my_string [0:6])
+
