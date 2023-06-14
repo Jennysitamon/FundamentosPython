51c73984ba2c7db7b845df82fb279423011cc331
jeny <jenymontiel13@gmail.com>
2023-06-14 11:27:45 -0600

    09 Tuplas y Conjuntos

---
+++ colecciones-2.py
@@ -0,0 +1,15 @@
+#Tuplas-corchetes
+my_tuple = ('Hola', 12, 1.5, True)
+print(type(my_tuple))
+print(my_tuple[0])
+print(my_tuple[-1])
+#Error
+#my_tuple[0] = "Otro valor" no permite duplicado solo una vez
+#Conjuntos(set)-llaves 
+my_set{1, 2, 3, 4, 4, 4, 4, 4, True, 'Hi'}
+print(type(my_set))
+print(my_set)
+
+lista =  [1, 2, 3, 1, 2, 3, 4,3, 5, 6]
+print(lista)
+print(set(lista))
