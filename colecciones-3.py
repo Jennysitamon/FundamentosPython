2976e552f41cf78d7e344a64062dcdba3d6ebdd4
jeny <jenymontiel13@gmail.com>
2023-06-14 11:39:56 -0600

    10Diccionarios

---
+++ colecciones-3.py
@@ -0,0 +1,13 @@
+teacher = {
+    "name": "Jenysita",
+    "l_name": "Lopez",
+    "phone": "234678955", 
+    "groups": ["3BDSM", "3BDSM"]
+}
+print(type(teacher))
+print(teacher["name"])
+print(teacher["groups"])
+teacher["groups"].append("3BDSM")
+teacher["phone"] = "34567812"
+teacher["city"] = "Huamantla"
+print(teacher)
